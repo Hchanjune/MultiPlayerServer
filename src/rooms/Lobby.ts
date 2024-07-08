f428507904728bc47d25111ce5da53d833a09922
Hchanjune <99157556+Hchanjune@users.noreply.github.com>
2024-07-08 18:51:03 +0900

    Update Lobby.ts

--- src/rooms/Lobby.ts
+++ src/rooms/Lobby.ts
@@ -4,13 +4,14 @@ import {LobbyService} from "../services/lobby/LobbyService";
 import {LobbyLoginOption} from "../options/lobby/LobbyLoginOption";
 
 export enum LobbyRequest {
-    "CREATE_ACCOUNT" = "CREATE_ACCOUNT"
+    CREATE_ACCOUNT = "CREATE_ACCOUNT",
+    CREATE_CUSTOM_SESSION = "CREATE_CUSTOM_SESSION"
 }
 
 export enum LobbyResponse {
-    "CONNECTED" = "CONNECTED",
-    "NOT_AUTHENTICATED" = "NOT_AUTHENTICATED",
-    "DUPLICATED_CONNECTION" = "DUPLICATED_CONNECTION"
+    CONNECTED = "CONNECTED",
+    NOT_AUTHENTICATED = "NOT_AUTHENTICATED",
+    DUPLICATED_CONNECTION = "DUPLICATED_CONNECTION"
 }
 
 export class Lobby extends Room<LobbyState> {
@@ -21,6 +22,24 @@ export class Lobby extends Room<LobbyState> {
         super();
         this.setState(new LobbyState());
         this.lobbyService = new LobbyService(this.state);
+
+        this.onMessage("*", (client, type, message) => {
+            this.handleMessage(client, type as LobbyRequest, message);
+        });
+    }
+
+    private handleMessage(client: Client, type: LobbyRequest, message: any) {
+        //console.log(`[Lobby] Received message of type ${type} from ${this.state.players.get(client.sessionId)?.id}:`, message);
+        switch (type) {
+
+            case LobbyRequest.CREATE_CUSTOM_SESSION:
+                console.log(message);
+                break;
+
+            default:
+                console.log(`Unknown message type: ${type}`);
+
+        }
     }
 
     onCreate(options: any) {
